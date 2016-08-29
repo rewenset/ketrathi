5ba1420f8bfbea0b0447bc2222a3af0462a5f5b8
rewenset <a.zakurenyi@gmail.com>
2016-08-29 17:20:24 +0300

    Added IMDb rating and title check using OMDb API

--- movie/forms.py
+++ movie/forms.py
@@ -9,3 +9,23 @@ class AddMovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = ['original_title', 'seen_for_the_first_time', 'poster']
+
+
+    def clean(self):
+        import urllib.request
+        import json
+        cleaned_data = super(AddMovieForm, self).clean()
+        title = cleaned_data.get('original_title')
+        title = title.replace(' ', '+')
+        url = 'http://www.omdbapi.com/?t={}&y=&plot=short&r=json'.format(title)
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+        resp_data = resp.read()
+
+        data = json.loads(resp_data.decode('utf-8'))
+
+        if 'Error' in data.keys():
+            print(data['Error'])
+            raise forms.ValidationError(data['Error'])
+
+        return cleaned_data
--- movie/models.py
+++ movie/models.py
@@ -5,9 +5,24 @@ from django.core.urlresolvers import reverse
 
 class Movie(models.Model):
     user = models.ForeignKey(User, default=1)
-    original_title = models.CharField(max_length=500)
+    original_title = models.CharField(max_length=200)
     seen_for_the_first_time = models.DateField()
     poster = models.FileField()
+    imdbRating = models.FloatField(default=0)
 
     def get_absolute_url(self):
         return reverse('movie:detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        import urllib.request
+        import json
+        title = str(self.original_title)
+        title = title.replace(" ", "+")
+        url = 'http://www.omdbapi.com/?t={}&y=&plot=short&r=json'.format(title)
+        url = url.replace(' ', '+')
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+        resp_data = resp.read()
+        data = json.loads(resp_data.decode('utf-8'))
+        self.imdbRating = data['imdbRating']
+        super(Movie, self).save(*args, **kwargs)
