a024d387900807fa73567a77a3fc1e1c4b374ab3
rewenset <a.zakurenyi@gmail.com>
2016-08-31 11:26:21 +0300

    Fixed stupid idea - storing IMDb rating in DB

--- movie/urls.py
+++ movie/urls.py
@@ -6,6 +6,6 @@ app_name = 'movie'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^movie/add/$', views.MovieCreate.as_view(), name='add'),
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.MovieDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/delete/$', views.MovieDelete.as_view(), name='delete'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -13,22 +13,36 @@ class IndexView(generic.ListView):
     context_object_name = 'all_movies'
     paginate_by = 8
 
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['username'] = self.request.user.username
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super(IndexView, self).get_context_data(**kwargs)
+    #     context['username'] = self.request.user.username
+    #     return context
 
     def get_queryset(self):
         return Movie.objects.filter(user=self.request.user).order_by('-seen_for_the_first_time')
 
 
 @method_decorator(login_required(login_url='/'), name='dispatch')
-class DetailView(generic.DetailView):
+class MovieDetailView(generic.DetailView):
     model = Movie
     template_name = 'movie/detail.html'
 
     def get_queryset(self):
         return Movie.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        import urllib.request
+        import json
+        context = super(MovieDetailView, self).get_context_data(**kwargs)
+        title = self.object.original_title
+        title = title.replace(" ", "+")
+        url = 'http://www.omdbapi.com/?t={}&y=&plot=short&r=json'.format(title)
+        req = urllib.request.Request(url)
+        resp = urllib.request.urlopen(req)
+        resp_data = resp.read()
+        data = json.loads(resp_data.decode('utf-8'))
+        context['imdbRating'] = data['imdbRating']
+        return context
 
 
 @method_decorator(login_required(login_url='/'), name='dispatch')
